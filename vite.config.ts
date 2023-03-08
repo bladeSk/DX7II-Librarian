16d9066b2ba7a785daeda57b75ae50e18bac617b
Matus Koprda <blade@blade.sk>
2023-03-08 09:47:23 +0100

    Update dependencies and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import path from 'path'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({ tsDecorators: true }),
+    tsconfigPaths(),
+    svgr({ exportAsDefault: true }),
+  ],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      'theme/': path.resolve(__dirname, './src/theme') + '/',
+    },
+  }
 })
