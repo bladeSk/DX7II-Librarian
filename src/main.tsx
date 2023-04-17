28f0ec17e58813bc8828ef633a56b0adc3d5448a
Matus Koprda <blade@blade.sk>
2023-04-17 11:29:52 +0200

    Fix CSS import order

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import './theme/theme.scss'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './theme/theme.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
